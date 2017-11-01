d43fcee7b5bd5185ee5259d4e4d79e13ecf332ec
ji.wenjiang <513755565@qq.com>
2017-11-01 11:32:43 +0800

    add rxjs && router

--- src/components/FromEvent.vue
+++ src/components/FromEvent.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Preview</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank"><router-link to="/FromEvent">From Event</router-link></a></li>
+      <li><a href="https://vuejs.org" target="_blank"><router-link to="/fromEvent">From Event</router-link></a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
@@ -21,12 +21,12 @@
 </template>
 
 <script>
-//  import Rx from 'rxjs/Rx'
+  //  import Rx from 'rxjs/Rx'
   export default {
-    name: 'HelloWorld',
+    name: 'FromEvent',
     data () {
       return {
-        msg: 'Welcome to Practice Rxjs on Vue'
+        msg: 'Welcome to Practice fromEvent of Rxjs'
       }
     },
     mounted () {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import FromEvent from '@/components/FromEvent'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/FromEvent',
+      name: 'FromEvent',
+      component: FromEvent
     }
   ]
 })
